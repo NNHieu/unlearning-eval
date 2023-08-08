fcb558d3af429ac046b7669d82590a05eecfad01
Dinh Phuong <bonjoureko1122000@gmail.com>
2023-08-08 09:16:30 +0000

    Fixed unproper probabilites return

--- RetainBasedEvaluator.py
+++ RetainBasedEvaluator.py
@@ -31,16 +31,28 @@ class ZeroRetrainForgetting(RetainBaseEvaluator):
     """
     def __init__(self, forget_set, test_set, base_model: nn.Module):
         super().__init__(forget_set, test_set, base_model)
+        self.norm = False
+    
+    # Set norm to true if the classification do not return probabilities of each class (0 < class_prob < 1, sum(class_prob) = 1)
+    def set_norm(self, value:bool):
+        self.norm = value
     
     def eval(self, unlearn_model: nn.Module):
         unlearn_forget_prob = unlearn_model(self.forget_set)
+        if self.norm:
+            unlearn_forget_prob = torch.sigmoid(unlearn_forget_prob)
+            unlearn_forget_prob = torch.softmax(unlearn_forget_prob, dim=1)
+        
         base_forget_prob = self.base_model(self.forget_set)
+        if self.norm:
+            base_forget_prob = torch.sigmoid(base_forget_prob)
+            base_forget_prob = torch.softmax(base_forget_prob, dim=1)
 
         zrf = 0
         for idx in range(len(self.forget_set)):
             avg = 0.5 * (unlearn_forget_prob[idx] + base_forget_prob[idx])
             js = 0.5 * (rel_entr(unlearn_forget_prob[idx], avg) + rel_entr(base_forget_prob[idx], avg))
             zrf += js
-        print(zrf)
+        
         zrf = zrf / len(self.forget_set)
         return 1 - zrf
