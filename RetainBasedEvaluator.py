200fe76bb32cee4b2069710fe9ff3c9455282a28
NNHieu <ngochieutb13@gmail.com>
2023-08-08 07:20:22 +0000

    pipeline cifar

--- RetainBasedEvaluator.py
+++ RetainBasedEvaluator.py
@@ -12,14 +12,14 @@ class ActivationDistance(RetainBaseEvaluator):
     def __init__(self, forget_set, test_set, base_model: nn.Module):
         super().__init__(forget_set, test_set, base_model)
 
-    def eval(self, unlearn_model: nn.Module):
+    def eval(self, unlearn_model: nn.Module, **kwargs):
         l2_loss = 0
         norm = 0
         for (k, p), (k_base, p_base) in zip(unlearn_model.named_parameters(), self.base_model.named_parameters()):
             # print(k)
             if p.requires_grad:
-                l2_loss += (p - p_base).pow(2).sum()
-                norm += p.pow(2).sum()
+                l2_loss += (p - p_base).pow(2).sum().cpu()
+                norm += p.pow(2).sum().cpu()
         return {"l2_loss": l2_loss, "norm_loss": torch.sqrt(l2_loss/norm)}
 
 
@@ -31,10 +31,17 @@ class ZeroRetrainForgetting(RetainBaseEvaluator):
     """
     def __init__(self, forget_set, test_set, base_model: nn.Module):
         super().__init__(forget_set, test_set, base_model)
-    
-    def eval(self, unlearn_model: nn.Module):
+
+    @torch.no_grad()
+    def eval(self, unlearn_model: nn.Module, *, device, **kwargs):
+        self.forget_set = self.forget_set.to(device)
+
         unlearn_forget_prob = unlearn_model(self.forget_set)
+        unlearn_forget_prob = torch.softmax(unlearn_forget_prob, dim=1).cpu().numpy()
+        
         base_forget_prob = self.base_model(self.forget_set)
+        base_forget_prob = torch.softmax(base_forget_prob, dim=1).cpu().numpy()
+
 
         zrf = 0
         for idx in range(len(self.forget_set)):
---
+++ attack_based.py
@@ -0,0 +1,69 @@
+import torch
+from sklearn import linear_model, model_selection
+import numpy as np
+from base import BaseEvaluator
+
+
+def compute_losses(net, loader, criterion, device):
+    """Auxiliary function to compute per-sample losses"""
+
+    all_losses = []
+
+    for inputs, targets in loader:
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        logits = net(inputs)
+        losses = criterion(logits, targets).numpy(force=True)
+        for l in losses:
+            all_losses.append(l)
+
+    return np.array(all_losses)
+
+def _simple_mia(sample_loss, members, n_splits=10, random_state=0):
+    """Computes cross-validation score of a membership inference attack.
+
+    Args:
+      sample_loss : array_like of shape (n,).
+        objective function evaluated on n samples.
+      members : array_like of shape (n,),
+        whether a sample was used for training.
+      n_splits: int
+        number of splits to use in the cross-validation.
+    Returns:
+      scores : array_like of size (n_splits,)
+    """
+
+    unique_members = np.unique(members)
+    if not np.all(unique_members == np.array([0, 1])):
+        raise ValueError("members should only have 0 and 1s")
+
+    attack_model = linear_model.LogisticRegression()
+    cv = model_selection.StratifiedShuffleSplit(
+        n_splits=n_splits, random_state=random_state
+    )
+    return model_selection.cross_val_score(
+        attack_model, sample_loss, members, cv=cv, scoring="accuracy"
+    )
+
+def simple_mia(model, criterion, forget_loader, test_loader, device, *, n_splits=10, random_state=0):
+    with torch.no_grad():
+        forget_losses = compute_losses(model, forget_loader, criterion, device=device)
+        test_losses = compute_losses(model, test_loader, criterion, device=device)
+    samples_mia = np.concatenate((test_losses, forget_losses)).reshape((-1, 1))
+    labels_mia = [0] * len(test_losses) + [1] * len(forget_losses)
+    mia_scores = _simple_mia(samples_mia, labels_mia, n_splits=n_splits, random_state=random_state)
+    return mia_scores
+
+class SimpleMiaEval(BaseEvaluator):
+    """
+    Simple MIA Evaluation
+    """
+    def __init__(self, forget_loader, test_loader, criterion, n_splits=10, random_state=0):
+        self.forget_loader = forget_loader
+        self.test_loader = test_loader
+        self.criterion = criterion
+        self.n_splits = n_splits
+        self.random_state = random_state
+
+    def eval(self, unlearn_model: torch.nn.Module, *, device, **kwargs):
+        return simple_mia(unlearn_model, self.criterion, self.forget_loader, self.test_loader, device, n_splits=self.n_splits, random_state=self.random_state)
